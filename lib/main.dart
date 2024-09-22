0be070090f237f1d272e3338eefb0622bb2e81d2
IMisbahK <m.misbahkhursheed@gmail.com>
2024-09-22 23:37:56 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  String user = "Misbah";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: 
-        Center(
-          child:
-          ElevatedButton(
-            child: Icon(Icons.laptop),
-            onPressed: (){
-              Navigator.push(context, 
-              MaterialPageRoute(
-                builder:
-                  (context) => const First()
-               )
-               );
-            },
-          ),
-          )
-      );
-  }
-}
+       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hey $user"),
+          leading: const Icon(Icons.menu),
+          actions: const [Icon(Icons.logout)],
+        ),
+        body: 
+        ListView(
+          children: [
+            Container(
+              height: 500,
+              width: 500,
+              color: const Color.fromARGB(255, 49, 1, 1),
+              child: Center(child:Text("Box 1")),
+            ),
+            Container(
+              height: 500,
+              width: 500,
+              color: const Color.fromARGB(255, 146, 12, 12),
+              child: Center(child:  Text("Box 2")),
+            ),
+            Container(
+              height: 500,
+              width: 500,
+              color: const Color.fromARGB(255, 234, 123, 123),
+              child: Center(
+                child:Text("Box 3")),
 
-class First extends StatelessWidget {
-  const First({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+            )
+          ],
+        )
+        ,
+       ),
     );
   }
 }
